703e4cf2359926997547076e79141c0638da3c8d
crazyzhanyue <crazyzhanyue@163.com>
2018-01-18 22:33:14 +0800

    up

---
+++ js/main.js
@@ -0,0 +1,17 @@
+;(function(w,d){
+	var body = d.body,
+	query = d.querySelector.bind(d),
+	queryAll = d.querySelectorAll.bind(d),
+	root = query('html'),
+	gotop = query('#gotop'),
+	menu = query('#menu'),
+	header = query('#header'),
+	mask = query('#mask'),
+	menuToggle = query('#menu-toggle'),
+	menuOff = $('#menu-off'),
+	loading = $('#loading'),
+	animate = w.requestAnimationFrame,
+	scrollSpeed = 200 / (1000 / 60);
+
+
+})(window,document);
