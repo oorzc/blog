2b0b2cdc0cec83198745fb80a9656a78044f375e
crazyzhanyue <crazyzhanyue@163.com>
2018-01-19 15:52:09 +0800

    up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,141 @@
 ;(function(w,d){
+	'use strict';
 	var body = d.body,
 	query = d.querySelector.bind(d),
 	queryAll = d.querySelectorAll.bind(d),
 	root = query('html'),
-	gotop = query('#gotop'),
 	menu = query('#menu'),
 	header = query('#header'),
 	mask = query('#mask'),
-	menuToggle = query('#menu-toggle'),
-	menuOff = $('#menu-off'),
-	loading = $('#loading'),
+	loading = query('#loading'),
+	menuOpen = query('#menu-open'),
+	menuOff = query('#menu-off'),
+	gotop = query('#gotop'),
 	animate = w.requestAnimationFrame,
-	scrollSpeed = 200 / (1000 / 60);
+	scrollSpeed = 20,
+	forEach = Array.prototype.forEach,
+	even = ('ontouchstart' in w && /Mobile|android|iOS|iPhone|iPad|Windows Phone|KEAPWI/i.test(navigator.userAgent)) ? 'touchstart' : 'click',
+	isWX = /micromessenger/i.test(navigator.userAgent),
+	noop = function () {},
+	offset = function (el) {
+		var x = el.offsetLeft,
+		y = el.offsetTop;
+		if (el.offsetParent) {
+			var pOfs = offset(el.offsetParent);
+			x += pOfs.x;
+			y += pOfs.y;
+		}
+		return {
+			x : x,
+			y : y
+		}
+	},
+	docEl = d.documentElement;
+	var Blog = {
+		//关闭显示侧边栏
+		toggleMenu : function (flag) {
+			var main = query('#main');
+			if (flag) {
+				menu.classList.remove('hide');
+				if (w.innerWidth < 1241 ) {
+					menu.classList.add('show');
+					mask.classList.add('in');
+					root.classList.add('lock');
+				}
+			}else{
+				menu.classList.remove('show');
+				mask.classList.remove('in');
+				root.classList.remove('lock');
+			}
+		},
+		//遮罩层
+		hideOnMask : [],
+		model :function (target) {
+			this._model = query(target);
+			this._off = this._model.querySelector('.close');
+
+			var self = this;
+			this.show = function () {
+				mask.classList.add('in');
+				self._model.classList.add('ready');
+				setTimeout(function () {
+					self._model.classList.add('in');
+				}, 20);
+			}
+			this.hide = function () {
+				mask.classList.remove('in');
+				self._model.classList.remove('ready');
+				setTimeout(function () {
+					self._model.classList.remove('in');
+				}, 20);
+			}
+			this.toggle = function () {
+				return self._model.classList.contains('in') ? self.hide() : self.show();
+			}
+			Blog.hideOnMask.push(this.hide);
+			this._off && this._off.addEventListener(even,  self.hide());
+
+		},
+
+	}
+
+	//页面加载完成后
+	w.addEventListener('load', function () {
+		// Blog.toggleMenu(true);
+		loading.classList.remove('active');
+		var top = docEl.scrollTop;
+
+	}, false);
+
+
+	//页面滚动时
+	w.addEventListener('scroll', function () {
+		var EloffsetTop = outils.getScrollTop();
+
+		setTimeout(function () {
+			if (EloffsetTop < 600) {
+				gotop.classList.remove('in');
+
+			}else{
+				gotop.classList.add('in');
+			}
+		}, 300);
+
+	}, false);
+
+	//页面缩放
+	w.addEventListener('resize', function () {
+
+
+	}, false);
+
+	//打开侧边栏
+	menuOpen.addEventListener(even, function (e) {
+		Blog.toggleMenu(true);
+		e.preventDefault();
+	}, false);
+
+	//关闭侧边栏
+	menuOff.addEventListener(even, function (e) {
+		menu.classList.add('hide');
+		e.preventDefault();
+	}, false);
+
+	mask.addEventListener(even, function (e) {
+		Blog.toggleMenu();
+		e.preventDefault();
+	}, false);
+
+
+
+	//返回顶部
+	gotop.addEventListener('click', function () {
+		outils.scrollTo(0, 300);
+	}, false);
+
+
+
+
 
 
 })(window,document);
